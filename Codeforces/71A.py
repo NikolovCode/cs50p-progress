9495ecde4f0068f0592b3cd272c40bf10b4e6014
NikolovCode <ngnikolov98@gmail.com>
2025-08-24 19:53:04 +0300

    CS50 update 2025-08-24 19:53:04

--- Codeforces/71A.py
+++ Codeforces/71A.py
@@ -1,12 +1,3 @@
-n = int(input(""))
-result = []
-while n > 0:
-    w = input("")
-    first, last = w[0] ,w[-1]
-    if len(w) > 10:
-        result.append(f"{first}{len(w)-2}{last}")
-    else:
-        result.append(w)
-    n -= 1
-for res in result:
-    print(res)    
+import emoji
+print(emoji.emojize("hello, :3rd_place_medal:"))
+
